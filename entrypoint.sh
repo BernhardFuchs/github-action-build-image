05514a019827941bc1d7c5cb79b912b8d88b7042
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2020-09-23 20:38:30 +0200

    fix: enhance logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
+echo "Dockerfile: $INPUT_PATH_TO_DOCKERFILE"
+echo "Name: $INPUT_CONTAINER_NAME"
 docker build . -f "$INPUT_PATH_TO_DOCKERFILE" -t "$INPUT_CONTAINER_NAME"
 docker images
