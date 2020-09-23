e97c051ca62615fe74bd4741c346318b2825b90f
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2020-09-23 19:26:16 +0200

    fix: remove image tag as it is not needed during build step

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-docker build . -f "$INPUT_PATH_TO_DOCKERFILE" -t "$INPUT_CONTAINER_NAME:$INPUT_CONTAINER_TAG"
+docker build . -f "$INPUT_PATH_TO_DOCKERFILE" -t "$INPUT_CONTAINER_NAME"
 docker images
