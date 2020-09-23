3f45fe306e76bc1a0cfbff936aaebb31fa4f06c0
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2020-09-23 20:47:06 +0200

    fix: correct name for env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 echo "Dockerfile: $INPUT_PATH_TO_DOCKERFILE"
-echo "Name: $INPUT_CONTAINER_NAME"
-docker build . -f "$INPUT_PATH_TO_DOCKERFILE" -t "$INPUT_CONTAINER_NAME"
+echo "Name: $INPUT_IMAGE_NAME"
+docker build . -f "$INPUT_PATH_TO_DOCKERFILE" -t "$INPUT_IMAGE_NAME"
 docker images
